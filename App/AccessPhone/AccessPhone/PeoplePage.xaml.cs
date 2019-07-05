1b03247ddf4cfa8d2a65b990d655e40f927c2b2c
Stephen Hawley <stephen.hawley@xamarin.com>
2019-07-05 16:33:24 -0400

    Added people support

--- App/AccessPhone/AccessPhone/PeoplePage.xaml.cs
+++ App/AccessPhone/AccessPhone/PeoplePage.xaml.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Plugin.ContactService;
+using Plugin.ContactService.Shared;
 using Xamarin.Forms;
+using System.Linq;
+
 
 namespace AccessPhone {
 	public partial class PeoplePage : ContentPage {
+		ObservableCollection<Contact> contactsCollection;
+		Contact currentContact = null;
 		public PeoplePage ()
 		{
 			InitializeComponent ();
+			GetContacts ();
+			listView.ItemTapped += OnTapped;
+		}
+
+
+		async Task GetContacts ()
+		{
+			var contacts = await CrossContactService.Current.GetContactListAsync();
+			contacts.OrderBy (ct => ct.Name);
+			listView.BindingContext = contacts;
+		}
+
+
+		void OnTapped (object sender, ItemTappedEventArgs args)
+		{
 		}
 	}
 }
