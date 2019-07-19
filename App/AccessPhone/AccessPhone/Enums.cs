497683c7edf4e0e2b25db73469847481cc8942c2
Stephen Hawley <stephen.hawley@xamarin.com>
2019-07-19 16:13:41 -0400

    Bind button availability.

--- App/AccessPhone/AccessPhone/Enums.cs
+++ App/AccessPhone/AccessPhone/Enums.cs
@@ -6,7 +6,7 @@ namespace AccessPhone {
 	public enum Permission {
 		Allowed, // you can do it
 		Disallowed, // you can't do it and can't see it
-		Unavailable, // you can't do it right now
+		UnavailableNow, // you can't do it right now
 	}
 }
 
